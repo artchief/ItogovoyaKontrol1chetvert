ac9b2b019d10b910cbb4926a534996065d357769
Artyom Khachaturyan <artchief@gmail.com>
2023-01-17 15:33:13 +0300

    align the code (shift+alt+F)

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.");
-string[] array1 = new string[4] {"Hello", "2", "world", ":-("};
+string[] array1 = new string[4] { "Hello", "2", "world", ":-(" };
 string[] array2 = new string[array1.Length];
 int count = 0;
 
-for(int i = 0; i < array1.Length; i++)
+for (int i = 0; i < array1.Length; i++)
 {
-    if(array1[i].Length <= 3)
+    if (array1[i].Length <= 3)
     {
         array2[count] = array1[i];
         count++;
